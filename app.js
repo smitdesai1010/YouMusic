f1d673423b6b523494d647ad8eb200cb0946d05d
Smit Desai <57582187+smitdesai1010@users.noreply.github.com>
2020-12-01 13:10:23 -0500

    parsed the output

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
-document.getElementById("abc").innerHTML = "Hello World";
 
-const Http = new XMLHttpRequest();
-const url='https://www.yt-download.org/api/button/mp3/9iIX4PBplAY';
-Http.open("GET", url);
-Http.send();
+var form = document.getElementsByClassName('form-inline')[0].addEventListener( 'submit' , (e) => { 
+    e.preventDefault() 
+    geturl();   
+} )
 
-Http.onreadystatechange = (e) => {
-    document.getElementById('a').innerHTML = Http.responseText;
-    console.log('ok')
+function geturl(){
+    var url = document.getElementsByClassName('form-control')[0].value;
+    url = url.split("/",4)[3];
+    //console.log(url);
+    request(url)
 }
 
+
+function request( url ){
+
+    const Http = new XMLHttpRequest();
+    url='https://www.yt-download.org/api/button/mp3/'+url;
+    Http.open("GET", url);
+    Http.send();
+    
+    Http.onreadystatechange = (e) => {
+        var a = Http.responseText;
+
+        a = a.slice( a.indexOf('div') )
+        a = a.slice( a.indexOf('https') )
+        a = a.split('"',1)
+        console.log(a);
+
+        document.getElementById('output').innerHTML = a;
+
+        const audio = document.createElement('audio');
+        audio.src = a;
+       // audio.play();
+    }  
+}
+
+
+
 //https://youtu.be/9iIX4PBplAY
+
