b5afa44630d41db9c371c3a058cf08edda48a46b
Smit Desai <57582187+smitdesai1010@users.noreply.github.com>
2020-12-19 02:29:11 -0500

    commit

--- js/getcontent.js
+++ js/getcontent.js
@@ -2,8 +2,6 @@ import { generateframes } from "./generatecontent.js";
 
 function start(input){
 
-    var key2 = prompt("Enter Key")
-    var key = 'AIzaSyClCgBnyLGw7'+key2
     var api = 'https://www.googleapis.com/youtube/v3/search?part=snippet&key=' + 
                key + '&type=video&maxResults=10&q=' + input;
 
