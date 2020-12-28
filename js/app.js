4398d5f2675e11451e7372a479961d7356a0670e
Smit Desai <57582187+smitdesai1010@users.noreply.github.com>
2020-12-28 13:54:35 -0500

    c

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ document.getElementsByClassName('form-inline')[0]
     .addEventListener( 'submit' , (e) => { 
         e.preventDefault() 
         var input = document.getElementsByClassName("form-control")[0].value;
+        document.querySelector('#CE').style.visibility = "hidden";
         start(input)
 
 } )
