04b187fc627bf880515f280da24a832172a729f1
Smit Desai <57582187+smitdesai1010@users.noreply.github.com>
2021-05-11 10:54:13 -0400

    added send btn

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,13 @@
 import {start} from './getcontent.js'
 
+document.getElementById('submit')
+    .addEventListener('click', (e) => {
+        e.stopPropagation();
+        var input = document.getElementById("form-control").value;
+    
+        start(input+' song')
+})
+
 document.getElementsByClassName('form-inline')[0]
     .addEventListener( 'submit' , (e) => { 
         e.preventDefault() 
